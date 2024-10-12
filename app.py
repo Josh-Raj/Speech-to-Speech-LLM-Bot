9dbeb0bc844b48dbc78a7686096bbaea84976f73
Josh Raj <joshuachrisraj@gmail.com>
2024-10-12 18:44:48 +0530

    2nd Commit

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ import ollama
 import warnings
 
 engine = pyttsx3.init()
-# engine.runAndWait()
 # engine.stop()
 
 # """ RATE"""
@@ -41,7 +40,7 @@ engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 f
 
 
 template = """
-You are a helpful AI assistant and provide the answer for the question asked in a short and concise way.
+You are a helpful AI assistant that provides the answer for the question asked in a short and concise way.
 
 Here is the conversation history: {context}
 
@@ -54,13 +53,12 @@ prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
 def recordUser():
+
   print("Listening..")
   recognizer = sr.Recognizer()
   with sr.Microphone() as source:
     recognizer.adjust_for_ambient_noise(source, duration=0.15)
     recognizer.pause_threshold=1
-    # print(f"Say Something for {time} seconds")
-    # heardAudio = recognizer.listen(source, timeout=time)
     heardAudio = recognizer.listen(source)
     print("Done")
   return heardAudio
@@ -104,15 +102,10 @@ def recordUser():
 #     return 'audio.wav created successfully'
 #     # return 0
 def convertSpeechToText(audioResponse):
-  # audio_file = path.join(path.dirname(path.realpath('__file__')), "audio.wav")
   recognizer = sr.Recognizer()
-  # with sr.AudioFile(audioResponse) as source:
-  #   audio = r.record(source)      
-    #recognize = sr.Recognizer()
   try:    
     print("You: " +recognizer.recognize_google(audioResponse))
   except sr.UnknownValueError:
-    print("Google could not understand audio")
     return "Google could not understand audio" 
   except sr.RequestError as e:
     print("Google error; {0}".format(e))
@@ -120,31 +113,17 @@ def convertSpeechToText(audioResponse):
   return recognizer.recognize_google(audioResponse)
   
 
-def estimateDuration(text):
-    words_per_minute = 130  # Typical speaking speed
-    word_count = len(text.split())  # Count the number of words
-    duration = word_count / words_per_minute * 60  # Convert minutes to seconds
-    return int(duration)
+# def estimateDuration(text):
+#     words_per_minute = 130  # Typical speaking speed
+#     word_count = len(text.split())  # Count the number of words
+#     duration = word_count / words_per_minute * 60  # Convert minutes to seconds
+#     return int(duration)
 
     
 def convertTextToSpeech(text):
-  # tts = gTTS(text)
-  # tts.save('Output.mp3')
-  # sound_file = b"../Output.mp3"
-  # (Audio(sound_file, autoplay=True, rate=16000)) 
-  # audio = AudioSegment.from_mp3(sound_file)
-  # # audio = AudioSegment.from_file(sound_file,format="mp3")
-  # # play(audio)
-  # duration = len(audio) / 1000
-  # time.sleep(duration)
   engine.say(text)
   engine.runAndWait()
-  # duration = estimateDuration(text)
-  # time.sleep(duration)
-  # print(f"Estimated speech duration: {duration} seconds")
     
-  
-
 def llmResponse():
   context = ""
   
@@ -158,20 +137,11 @@ def llmResponse():
     print("AI: "+result)
     context += f"\nUser: {text}\nAI: {result}"
   convertTextToSpeech(result)
-  
-  #time.sleep(2)
-  # response = ollama.chat(model='llama3.2:1b', messages=[
-  # {
-  #   'role': 'user',
-  #   'content': 'Why is the sky blue?',
-  # },
-  # ])
-  # print(response['message']['content'])
-  
+
   return result
   
 def main():
-  warnings.filterwarnings("ignore", )
+  warnings.filterwarnings("ignore")
   while True:
     userInput = llmResponse()
     if userInput.lower() in ["stop", "exit", "quit"]:
